69257c421d51279c1084758ef6001800cb0d5f42
YWMKerman <78585019+YWMKerman@users.noreply.github.com>
2024-11-26 19:20:02 +0800

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿#define CIBUILD_disabled
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("The AfterSolarSystem Mod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("YWMKerman")]
+[assembly: AssemblyTrademark("YWM_Studio")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c44fc31a-d718-4d96-b789-91c4e9bfd950")]
+
+#if CIBUILD
+[assembly: AssemblyVersion("@MAJOR@.@MINOR@.@PATCH@.@BUILD@")]
+[assembly: AssemblyFileVersion("@MAJOR@.@MINOR@.@PATCH@.@BUILD@")]
+#else
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("114.514.191.9810")]
+#endif
+
